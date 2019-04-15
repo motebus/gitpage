0b77051e507812c0976b8a9f33869fc4cd5410f9
kevin3251 <herry3251@gmail.com>
2019-04-15 16:26:09 +0800

    fix project structure

--- template/src/main.js
+++ template/src/main.js
@@ -1,11 +1,9 @@
-
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import pageUtil from 'vue-gitpage'
 
 Vue.use(pageUtil)
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  render: h => h(App),
+}).$mount('#app')
